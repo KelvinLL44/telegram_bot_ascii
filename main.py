e9e9d9185c517df10192c61ff0388ca92d35afaa
kelvin <kelvinl@graymatics.com>
2025-01-07 17:19:35 +0800

    change token

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ logger = logging.getLogger(__name__)
 
 # Load environment variables
 load_dotenv()
-# TOKEN = '7243623278:AAFwvUaGyraIQTtj-ouiwbF3EZY-1YCFLNQ'  # Hardcoded for testing
-TOKEN = "7746073326:AAEVoCuTN6hfgkYbgN0Pm8KTL2qEkXetJKk"   # Kelvin's
+TOKEN = '7243623278:AAFwvUaGyraIQTtj-ouiwbF3EZY-1YCFLNQ'  # Hardcoded for testing
+# TOKEN = "7746073326:AAEVoCuTN6hfgkYbgN0Pm8KTL2qEkXetJKk"   # Kelvin's
 TOKEN_ADDRESS = '6t7heUCjsgxa5ZwFQyfZwY9cEm9ACMxjgJrH1MELpump'  # Hardcoded for testing
 BOT_TITLE = "ASScii art AI Agent"
 BOT_SUBTITLE = "Making Tech Sexier Than Your Girlfriend! 🔥🚨🚀"
