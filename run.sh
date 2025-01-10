2bcfa4465239b415fe2da4a09efd303bad70498d
kelvin <kelvin@chi.app>
2025-01-10 13:32:59 +0800

    change bot token

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ python3 main.py
 
 # docker build -t telegram-bot .
 # docker container rm --force telegram-bot-container 
-# docker run -d --name telegram-bot-container telegram-bot
+# docker run --network=host --name telegram-bot-container telegram-bot
